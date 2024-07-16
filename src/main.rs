f9416051a8cbd9fddbb67e5c44b8af01165569bf
wrvsrx <wrvsrx@outlook.com>
2024-07-16 10:21:49 +0800

    feat: support searching oneline heading

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,98 @@
-use std::cell::Cell;
+use std::fs;
+use std::process::exit;
 
+use dashmap::DashMap;
+use ropey::Rope;
 use tokio;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
-use tree_sitter::Parser;
+use tree_sitter::{Node, Parser, Tree};
 
 #[derive(Debug)]
 struct Backend {
     client: Client,
-    parser: Cell<Parser>
+    ast_map: DashMap<String, Tree>,
+    document_map: DashMap<String, Rope>,
+}
+
+impl Backend {
+    async fn on_change(&self, params: TextDocumentItem) {
+        let rope = ropey::Rope::from_str(&params.text);
+        self.document_map
+            .insert(params.uri.to_string(), rope.clone());
+        let mut parser = Parser::new();
+        parser
+            .set_language(&tree_sitter_djot::language())
+            .expect("Error loading djot grammer");
+        let tree = parser.parse(rope.to_string(), None).unwrap();
+        self.ast_map.insert(params.uri.to_string(), tree);
+    }
 }
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file opened!")
+            .await;
+        self.on_change(TextDocumentItem {
+            uri: params.text_document.uri,
+            language_id: params.text_document.language_id,
+            text: params.text_document.text,
+            version: params.text_document.version,
+        })
+        .await
+    }
+
+    async fn did_change(&self, mut params: DidChangeTextDocumentParams) {
+        self.on_change(TextDocumentItem {
+            uri: params.text_document.uri,
+            language_id: String::from("djot"),
+            text: std::mem::take(&mut params.content_changes[0].text),
+            version: params.text_document.version,
+        })
+        .await
+    }
+
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
         Ok(InitializeResult {
             capabilities: ServerCapabilities {
                 hover_provider: Some(HoverProviderCapability::Simple(true)),
                 completion_provider: Some(CompletionOptions::default()),
+                document_symbol_provider: Some(OneOf::Left(true)),
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::FULL,
+                )),
                 ..Default::default()
             },
             ..Default::default()
         })
+    }
+
+    async fn document_symbol(
+        &self,
+        params: DocumentSymbolParams,
+    ) -> Result<Option<DocumentSymbolResponse>> {
+        self.client
+            .log_message(MessageType::INFO, format!("{:?}", self.ast_map))
+            .await;
+
+        let tree = self
+            .ast_map
+            .get(&params.text_document.uri.to_string())
+            .unwrap();
+        let text = self
+            .document_map
+            .get(&params.text_document.uri.to_string())
+            .unwrap();
+        let mut cursor = tree.root_node().walk();
+        let symbols: Vec<DocumentSymbol> = tree
+            .root_node()
+            .children(&mut cursor)
+            .filter_map(|child| find_document_heading(child, &text))
+            .collect();
+        Ok(Some(DocumentSymbolResponse::Nested(symbols)))
     }
 
     async fn initialized(&self, _: InitializedParams) {
@@ -50,18 +120,82 @@ impl LanguageServer for Backend {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct Sections {
+    heading: String,
+    childern: Vec<Sections>,
+}
+
+fn find_document_heading(node: Node, text: &Rope) -> Option<DocumentSymbol> {
+    if node.kind() == "section" {
+        let first_child = node.child(0).unwrap();
+        // check if first_child.kind()'s prefix is "heading", dump the code
+        assert!(first_child.kind().starts_with("heading"));
+        assert_eq!(first_child.child(0).unwrap().kind(), "marker");
+        let range = first_child.child(1).unwrap().range();
+        let heading = text
+            .slice(text.byte_to_char(range.start_byte)..(text.byte_to_char(range.end_byte) - 1))
+            .to_string();
+        let mut cursor = node.walk();
+        let a: Vec<DocumentSymbol> = node
+            .child(1)
+            .unwrap()
+            .children(&mut cursor)
+            .filter_map(|child| find_document_heading(child, &text))
+            .collect();
+        let r2r = |range: tree_sitter::Range| -> Range {
+            return Range {
+                start: Position {
+                    line: range.start_point.row as u32,
+                    character: range.start_point.column as u32,
+                },
+                end: Position {
+                    line: range.end_point.row as u32,
+                    character: range.end_point.column as u32,
+                },
+            };
+        };
+        Some(DocumentSymbol {
+            name: heading,
+            detail: None,
+            kind: SymbolKind::NAMESPACE,
+            tags: None,
+            deprecated: None,
+            range: r2r(node.range()),
+            selection_range: r2r(first_child.range()),
+            children: if a.len() > 0 { Some(a) } else { None },
+        })
+    } else {
+        None
+    }
+}
+
 #[tokio::main]
 async fn main() {
     // let mut parser = Parser::new();
     // parser
     //     .set_language(&tree_sitter_djot::language())
     //     .expect("Error loading djot grammer");
-    // let source_code = "_This is regular_ not strong emphasis\n*strong*\n";
-    // let mut tree = parser.parse(source_code, None).unwrap();
+    // let source_code = fs::read_to_string("b.dj").unwrap();
+    // // "# Heading\n\nsomethind\n\n## Heading ne\n\n114514\n\n# Heading 2\n\n114514\n";
+    // let rope = Rope::from_str(&source_code);
+    // let tree = parser.parse(source_code, None).unwrap();
     // println!("{}", tree.root_node().to_sexp());
+    // let mut cursor = tree.root_node().walk();
+    // let hs: Vec<DocumentSymbol> = tree
+    //     .root_node()
+    //     .children(&mut cursor)
+    //     .filter_map(|child| find_document_heading(child, &rope))
+    //     .collect();
+    // println!("{:?}", hs);
+    // exit(0);
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::new(|client| Backend { client });
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        ast_map: DashMap::new(),
+        document_map: DashMap::new(),
+    });
     Server::new(stdin, stdout, socket).serve(service).await;
 }
---
+++ src/utils.rs
@@ -0,0 +1,6 @@
+fn offset_to_position(offset: usize, rope: &Rope) -> Option<Position> {
+    let line = rope.try_char_to_line(offset).ok()?;
+    let first_char_of_line = rope.try_line_to_char(line).ok()?;
+    let column = offset - first_char_of_line;
+    Some(Position::new(line as u32, column as u32))
+}
