9ae325220fd87c4b8db147c3aa0e0d4ac9ca467e
wrvsrx <wrvsrx@outlook.com>
2026-06-17 02:01:38 +0800

    dev: set Neovim workspace root

--- dev/nvim.lua
+++ dev/nvim.lua
@@ -1,6 +1,7 @@
 vim.lsp.config['djot-language-server'] = {
   cmd = { './target/debug/djot-ls' },
   filetypes = { 'djot' },
+  root_dir = vim.fs.root(0, { '.git' }),
 }
 
 vim.lsp.enable('djot-language-server')
